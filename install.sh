3cbdf87ab3468bac3929c8373d8bbf61ec1347bf
Bogstag <8104765+Bogstag@users.noreply.github.com>
2026-09-17 22:47:28 +0200

    feat: update chezmoi configuration templates and install script for improved environment handling

--- install.sh
+++ install.sh
@@ -16,14 +16,14 @@ if ! chezmoi="$(command -v chezmoi)"; then
 		echo "To install chezmoi, you must have curl or wget installed." >&2
 		exit 1
 	fi
-	sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
+	sh -c "$chezmoi_install_script" -- -b "$bin_dir"
 	unset chezmoi_install_script bin_dir
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}"
+set -- init --apply --source="$script_dir"
 
 echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
